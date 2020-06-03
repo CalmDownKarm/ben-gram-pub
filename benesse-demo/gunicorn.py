3fac9a9468295adbbc5257032326deb31ed39abf
calmdownkarm <karmanyaaggarwal@gmail.com>
2020-06-03 15:15:39 +0530

    flask app added

---
+++ benesse-demo/gunicorn.py
@@ -0,0 +1,57 @@
+bind = '127.0.0.1:5555'
+backlog = 2048
+
+workers = 2
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 120
+keepalive = 120
+spew = False
+
+# with open('.flaskenv') as flaskenv:
+  #  raw_env = flaskenv.readlines()
+
+raw_env = [
+     'SECRET_KEY=something',
+     'FLASK_APP=app.py',
+     'FLASK_DEBUG=0'
+ ]
+
+errorlog = '-'
+loglevel = 'info'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+proc_name = None
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+def pre_fork(server, worker):
+    pass
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+def worker_int(worker):
+    worker.log.info("worker received INT or QUIT signal")
+
+    ## get traceback info
+    import threading, sys, traceback
+    id2name = {th.ident: th.name for th in threading.enumerate()}
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""),
+            threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename,
+                lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    worker.log.debug("\n".join(code))
+
+def worker_abort(worker):
+    worker.log.info("worker received SIGABRT signal")
---
+++ benesse-demo/src/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from werkzeug.exceptions import HTTPException, default_exceptions
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    return app
+
+app = create_app()
+
+def handle_error(error):
+    if isinstance(error, HTTPException):
+        return jsonify(message=error.description, code=error.code)
+    return jsonify(message='An error occured', code=500)
+
+for exc in default_exceptions:
+    app.register_error_handler(exc, handle_error)
+
+from .api import *
+
+app.register_blueprint(api)
+app.add_template_global(name='version', f='1.0')
